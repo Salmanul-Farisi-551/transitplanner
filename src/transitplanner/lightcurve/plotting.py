987dc1c12b0152d18784e33ba9cf00fc9b55eb64
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-02-06 20:24:05 +0000

    Update plotting.py

--- src/transitplanner/lightcurve/plotting.py
+++ src/transitplanner/lightcurve/plotting.py
@@ -6,4 +6,6 @@ def plot_lightcurve(obstime, flux, title="Light Curve"):
     plt.xlabel("Observation Time (Hours)")
     plt.ylabel("Relative Flux")
     plt.title(title)
+    plt.savefig("lightcurve.png", dpi=150, bbox_inches="tight")
     plt.show()
+
