d49b373176b5ef329efc016f1327201d82e6b632
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-02-08 22:36:37 +0000

    Update geometry.py

--- src/transitplanner/core/geometry.py
+++ src/transitplanner/core/geometry.py
@@ -1,7 +1,7 @@
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 import astropy.units as u
 
-def radec_to_altaz(ra, dec, location, obstime):
-    coord = SkyCoord(ra=ra, dec=dec, unit=(u.hourangle, u.deg))
+def radec_to_altaz(coord, location, obstime):
     altaz = coord.transform_to(AltAz(obstime=obstime, location=location))
     return altaz.alt.deg, altaz.az.deg
+
