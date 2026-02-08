9b08d608bbdc0335a355bd5b9a0aecbfeebd88b0
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-02-08 22:50:09 +0000

    Update exoclock.py

--- src/transitplanner/io/exoclock.py
+++ src/transitplanner/io/exoclock.py
@@ -2,4 +2,6 @@ import urllib.request, json
 
 def load_exoclock_data():
     url = "https://www.exoclock.space/database/planets_json"
-    return json.loads(urllib.request.urlopen(url).read())
+    return  load_json(url)
+
+
