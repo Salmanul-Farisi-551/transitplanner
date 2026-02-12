1fe96f951ff2e3554782095bf0efd302b7072403
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-02-12 14:13:09 +0000

    Update nasa.py

--- src/transitplanner/io/nasa.py
+++ src/transitplanner/io/nasa.py
@@ -6,9 +6,11 @@ CACHE_FILE = Path.home() / ".transitplanner_nasa_cache.pkl"
 
 def load_nasa_data():
     if CACHE_FILE.exists():
+        print("LOADING FROM CACHE")
         with open(CACHE_FILE, "rb") as f:
             return pickle.load(f)
-
+            
+     print("LOADING FROM CACHE")
     table = NasaExoplanetArchive.query_criteria("pscomppars")
     df = table.to_pandas()
     df.set_index("pl_name", inplace=True)
@@ -17,3 +19,4 @@ def load_nasa_data():
         pickle.dump(df, f)
 
     return df
+
