f2ca2d6f9e1c1a4127df541c75ee9db8a503e966
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-02-12 13:39:20 +0000

    Update nasa.py

--- src/transitplanner/io/nasa.py
+++ src/transitplanner/io/nasa.py
@@ -1,7 +1,23 @@
+
+
+from pathlib import Path
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
+import pickle
+
+CACHE_FILE = Path.home() / ".transitplanner_nasa_cache.pkl"
 
 def load_nasa_data():
-    table = NasaExoplanetArchive.query_criteria("pscomppars")
-    df = table.to_pandas()
-    df.set_index('pl_name', inplace=True)
-    return df
+    if CACHE_FILE.exists():
+        with open(CACHE_FILE, "rb") as f:
+            return pickle.load(f)
+
+    table = NasaExoplanetArchive.query_criteria(
+        table="pscomppars",
+        select="pl_name,pl_orbper,pl_tranmid,pl_trandur",
+        where="pl_tranflag=1"
+    )
+
+    with open(CACHE_FILE, "wb") as f:
+        pickle.dump(table, f)
+
+    return table
