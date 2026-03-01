838f472b8cc60a94bac7bbdbe30d88dc9916fcb4
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-03-01 20:28:06 +0000

    Update ephemeris.py

--- src/transitplanner/core/ephemeris.py
+++ src/transitplanner/core/ephemeris.py
@@ -1,4 +1,5 @@
 # Compute transit times, periods, epochs
 def next_transit(mid_time, period_days, start_time):
     n = int((start_time - mid_time) / period_days)
-    return mid_time + (n + 1) * period_days
+    return mid_time + (n ) * period_days
+
